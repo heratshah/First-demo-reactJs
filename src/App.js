7644f99c0e622f50c6217169a9d79f74906db1a8
heratrhad <herat@rhad.agency>
2022-05-07 15:39:18 +0530

    first react push

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+
+import From from "./MyComponents/From";
+
+import { Footer } from "./MyComponents/Footer";
+
+function App() {
+  return (
+    <>
+    <logo/>
+      <Header />
+      <Todos />
+      <From />
+      <Footer />
+     
+    </  >
+  );
+}
+
+
+export default App;
